48f58c5b6246d04ab9ab4ffe2c8b95aae2f277be
Alex <35805922+solarage@users.noreply.github.com>
2018-02-08 03:09:27 +0400

    added

---
+++ emount/js/script.js
@@ -0,0 +1,58 @@
+//Preloader
+
+
+document.body.onload = function() {
+	setTimeout(function() {
+		var preloader = document.getElementById('page-preloader')
+		if(!preloader.classList.contains('done')) {
+			preloader.classList.add('done');
+		}
+	}, 1000);
+}
+
+//Menu
+
+menu.onclick = function	openMenu() {
+	var x = document.getElementById("myTopnav");
+
+	if (x.className === "topnav") {
+		x.className += " responsive";
+	} else {
+		x.className = "topnav";
+	}
+}
+
+//Carousel
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+	showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+	showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+	var i;
+	var slides = document.getElementsByClassName("mySlides");
+	var dots = document.getElementsByClassName("dot");
+
+	if(n > slides.length) {
+		slideIndex = 1;
+	}
+	if(n < 1) {
+		slideIndex = slides.length;
+	}
+	for(i=0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	for (i=0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace("active", "");
+	}
+	slides[slideIndex-1].style.display = "block";
+	dots[slideIndex-1].className += "active";
+}
+
